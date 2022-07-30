eb6f8874e78e2feda20cbc144c8415d124dbdcc3
indymnv <indy.mnv@gmail.com>
2022-07-30 21:20:53 +0900

    improve viz life expectancy

--- eda.jl
+++ eda.jl
@@ -1,9 +1,40 @@
 using DataFrames, CSV
-using Plots
+using Gadfly
 
 df = DataFrame(CSV.File("001-eda-happines/data/2021.csv"))
 
-life_expectancy = select(df, "Country name", "Healthy life expectancy")
+life_expectancy = sort!(select(df, "Country name", "Healthy life expectancy"),
+		       :"Healthy life expectancy", 
+		       rev = true)
 
-bar(life_expectancy, y="Country name", x="Healthy life expectancy", kind = "bar")
-bar(df[:,"Country name"], df[:,"Healthy life expectancy"] , orientation = :v )
+#bar(life_expectancy, y="Country name", x="Healthy life expectancy", kind = "bar")
+bar(life_expectancy[:,"Country name"], 
+    life_expectancy[:,"Healthy life expectancy"] ,
+    title="Healthy Life expectancy",
+    xrotation = 50,
+    legend= false,
+    color = "red",
+    size= (1000,600)
+   )
+
+bar(first(life_expectancy[:,"Country name"], 10), 
+    first(life_expectancy[:,"Healthy life expectancy"],10) ,
+    title="Healthy Life expectancy",
+    xrotation = 50,
+    legend = false,
+    color = "green",
+    size= (1000,600)
+   )
+bar(last(life_expectancy[:,"Country name"], 10), 
+    last(life_expectancy[:,"Healthy life expectancy"],10) ,
+    title="Healthy Life expectancy",
+    xrotation = 50,
+    legend = false,
+    color = "red",
+    size= (1000,600)
+   )
+
+
+
+
+
